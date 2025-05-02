9a6292a03d0fb2e41f83cc16ea7b40deafa2eacc
SK <englishenglishenglish@proton.me>
2025-05-02 14:13:04 +0000

    Update 1__prefix sum technique.c

--- 1__prefix sum technique.c
+++ 1__prefix sum technique.c
@@ -4,17 +4,20 @@ int main(){
     int n;
     scanf("%d",&n);
 
+    //logic for special case
     if(n<1){
         printf("wrong input\n");
         return -1;
     }
 
+    
+    //logic for taking input
     int array[n];
     for(int i=0;i<n;i++){
         scanf("%d",&array[i]);
     }
 
-
+    //logic for building the prefix sum array
     int prefix[n];
     prefix[0]=array[0];
 
@@ -22,6 +25,8 @@ int main(){
         prefix[i]=prefix[i-1]+array[i];
     }
 
+
+    //logic for printing the array 
     for(int i=0;i<n;i++){
         printf("%d ",prefix[i]);
     }
